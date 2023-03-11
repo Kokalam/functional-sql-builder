6216f475301e83dfc59d4ffd8f95da76cbf0f9f7
stéphane yvon <os-stephane.yvon@rakuten.com>
2023-03-11 13:52:51 +0100

    :test_tube: update tests for functional builder

--- src/test/java/com/kokalam/sql/query/QueryTest.java
+++ src/test/java/com/kokalam/sql/query/QueryTest.java
@@ -1,155 +1,171 @@
-package com.kokalam.sql.request;
+package com.kokalam.sql.query;
 
-import com.kokalam.sql.request.column.Column;
-import com.kokalam.sql.request.table.Table;
+import com.kokalam.sql.query.instructions.select.column.Column;
+import com.kokalam.sql.query.instructions.from.clauses.table.Table;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SelectFromTest {
+public class QueryTest {
 
     @Test
     @DisplayName("select all column from table User")
     public void shouldReturnQueryWithAllColumnFromTableUser() {
         final String expectedQuery = "SELECT * FROM User";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select()
-                .from("User")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                    s.column(c -> c.name("*"))
+                ).from(f ->
+                    f.table(t -> t.name("User"))
+                ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name from table User")
     public void shouldReturnQueryWithOneColumnFromTableUser() {
         final String expectedQuery = "SELECT name FROM User";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select("name")
-                .from("User")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.name("name"))
+            )
+            .from(f ->
+                f.table(t -> t.name("User"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name and surname from table User")
     public void shouldReturnQueryWithTwoColumnFromTableUser() {
         final String expectedQuery = "SELECT name,surname FROM User";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select("name", "surname")
-                .from("User")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.name("name"))
+                 .column(c -> c.name("surname"))
+            ).from(f ->
+                f.table(t -> t.name("User"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name as pseudo from table User")
     public void shouldReturnQueryWithOneColumnAsFromTableUser() {
         final String expectedQuery = "SELECT name as pseudo FROM User";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select(Column.column("name").as("pseudo"))
-                .from("User")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.name("name").as("pseudo"))
+            ).from(f ->
+                f.table(t -> t.name("User"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name as pseudo and surname from table User")
     public void shouldReturnQueryWithOneColumnAsAndOneColumnFromTableUser() {
         final String expectedQuery = "SELECT name as pseudo,surname FROM User";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select(Column.column("name").as("pseudo"), Column.column("surname"))
-                .from("User")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.name("name").as("pseudo"))
+                 .column(c -> c.name("surname"))
+            ).from(f ->
+                f.table(t -> t.name("User"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name as pseudo and surname as family_name from table User")
     public void shouldReturnQueryWithTwoColumnAsFromTableUser() {
         final String expectedQuery = "SELECT name as pseudo,surname as family_name  FROM User";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select(
-                    Column.column("name").as("pseudo"),
-                    Column.column("surname").as("family_name")
-                )
-                .from("User")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.name("name").as("pseudo"))
+                 .column(c -> c.name("surname").as("family_name"))
+            ).from(f ->
+                f.table(t -> t.name("User"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name from table User as u and column street from table Address as a")
     public void shouldReturnQueryWithTwoColumnAsFromTwoTableWithAlias() {
         final String expectedQuery = "SELECT u.name,a.street FROM User u,Address a";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select(
-                        Column.column("u","name"),
-                        Column.column("a","street")
-                )
-                .from(Table.table("User").as("u"), Table.table("Address").as("a"))
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.of("u").name("name"))
+                 .column(c -> c.of("u").name("street"))
+            ).from(f ->
+                f.table(t -> t.name("User").as("u"))
+                 .table(t -> t.name("Address").as("a"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name from table User and column street from table Address")
     public void shouldReturnQueryWithTwoColumnAsFromTwoTable() {
         final String expectedQuery = "SELECT User.name,Address.street FROM User,Address";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select(
-                        Column.column("User","name"),
-                        Column.column("Address","street")
-                )
-                .from("User", "Address")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.of("User").name("name"))
+                 .column(c -> c.of("Address").name("street"))
+            ).from(f ->
+                f.table(t -> t.name("User"))
+                 .table(t -> t.name("Address"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("select column name as pseudo from table User and column street as location from table Address")
     public void shouldReturnQueryWithTwoColumnWithAliasAsFromTwoTable() {
         final String expectedQuery = "SELECT name as pseudo,street as location FROM User,Address";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select(
-                        Column.column("name").as("pseudo"),
-                        Column.column("street").as("location")
-                )
-                .from("User", "Address")
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.name("name").as("pseudo"))
+                 .column(c -> c.name("street").as("location"))
+            ).from(f ->
+                f.table(t -> t.name("User"))
+                 .table(t -> t.name("Address"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("should select column name as pseudo from table User as u and column street as location from table Address as a")
     public void shouldReturnQueryWithTwoColumnWithAliasAsFromTwoTableWithAlias() {
         final String expectedQuery = "SELECT u.name as pseudo,a.street as location FROM User,Address";
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.select(
-                        Column.column("u", "name").as("pseudo"),
-                        Column.column("a", "street").as("location")
-                )
-                .from(Table.table("User").as("u"), Table.table("Address").as("a"))
-                .build();
+        Query.Builder builder = new Query.Builder();
+        Query query = builder.select(s ->
+                s.column(c -> c.of("u").name("name").as("pseudo"))
+                 .column(c -> c.of("a").name("street").as("location"))
+            ).from(f ->
+                f.table(t -> t.name("User").as("u"))
+                 .table(t -> t.name("Address").as("a"))
+            ).build();
 
-        Assertions.assertEquals(expectedQuery, request.get());
+        Assertions.assertEquals(expectedQuery, query.get());
     }
 
     @Test
     @DisplayName("call from clause before select clause")
     public void shouldThrowIllegalStateExceptionWhenFromIsCalledBeforeSelect() {
-        Request.Builder builder = new Request.Builder();
-        Assertions.assertThrows(IllegalStateException.class, () -> builder.from("User"));
-        Assertions.assertThrows(IllegalStateException.class, () -> builder.from("User", "Address"));
-        Assertions.assertThrows(IllegalStateException.class, () -> builder.from(Table.table("User")));
-        Assertions.assertThrows(IllegalStateException.class, () -> builder.from(Table.table("User"), Table.table("Address")));
+        Query.Builder builder = new Query.Builder();
+        Assertions.assertThrows(IllegalStateException.class, () -> builder.from(f ->
+                f.table(t -> t.name("User").as("u"))
+        ));
     }
 }
